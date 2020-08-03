f0c6982a710037c2f752f7baa9f33738919814dd
Tristan Mark Nuck <nucktris@b-tu.de>
2020-08-03 23:23:32 +0200

    Camera Controlls

---
+++ Zaruma-Forgotten-Shards-UnityProject/Assets/Camera/CameraController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Vector2 mousePosition;
+    private Vector2 screenSpace;
+    public float speed;
+    public float offset;
+    private Vector2 direction;
+    private Vector2 directionKeyBoard;
+
+    public float minRotation;
+    public float maxRotation;
+    public float minHight;
+    public float maxHight;
+
+    private bool mouseMovement = false;
+
+
+    private float mouseWheel;
+    public float scrollSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M)){
+            mouseMovement = !mouseMovement;
+        }
+
+        checkMousePositionToScreen();
+
+        KeyBoardControlls();
+        
+        CalcRotation();
+
+    }
+
+    void FixedUpdate()
+    {
+        if(mouseMovement){
+            Camera.main.transform.position += new Vector3(direction.x,0,direction.y) * speed * Time.fixedDeltaTime;
+        }
+
+        Camera.main.transform.position += new Vector3(directionKeyBoard.x,0,directionKeyBoard.y) * speed * Time.fixedDeltaTime;
+    }
+
+    void checkMousePositionToScreen()
+    {
+        mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        screenSpace = new Vector2(Screen.width, Screen.height);
+
+        if(mousePosition.x >= screenSpace.x - offset){
+            direction = new Vector2(1, direction.y);
+        }else if(mousePosition.x <= 0 + offset){
+            direction = new Vector2(-1, direction.y);
+        }else{
+            direction = new Vector2(0,direction.y);
+        }
+
+        if(mousePosition.y >= screenSpace.y - offset){
+            direction = new Vector2(direction.x, 1);
+        }else if(mousePosition.y <= 0 + offset){
+            direction = new Vector2(direction.x, -1);
+        }else{
+            direction = new Vector2(direction.x, 0);
+        }
+    }
+
+    void KeyBoardControlls()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            directionKeyBoard = new Vector2(directionKeyBoard.x, 1);
+        }else if(Input.GetKey(KeyCode.S)){
+            directionKeyBoard = new Vector2(directionKeyBoard.x, -1);
+        }else{
+            directionKeyBoard = new Vector2(directionKeyBoard.x, 0);
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            directionKeyBoard = new Vector2(-1, directionKeyBoard.y);
+        }else if(Input.GetKey(KeyCode.D)){
+            directionKeyBoard = new Vector2(1, directionKeyBoard.y);
+        }else{
+            directionKeyBoard = new Vector2(0, directionKeyBoard.y);
+        }
+    }
+
+    void CalcRotation()
+    {
+        mouseWheel = Input.mouseScrollDelta.y;
+        transform.position = new Vector3(transform.position.x, 
+                                         Mathf.Clamp(transform.position.y + mouseWheel * scrollSpeed * -10 * Time.fixedDeltaTime, minHight, maxHight), 
+                                         transform.position.z);
+        
+        float deltaRotation = maxRotation - minRotation;
+        float deltaHight = maxHight - minHight;
+        float currentHight = maxHight - transform.position.y;
+        float percent = currentHight / deltaHight;
+        transform.eulerAngles = new Vector3(minRotation + deltaRotation * (1-percent), transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+}
---
+++ Zaruma-Forgotten-Shards-UnityProject/Assets/Camera/CameraControllerEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraController))]
+public class CameraControllerEditor : Editor
+{
+
+    SerializedProperty speed, offset, minRotation, maxRotation;
+
+    void OnEnable()
+    {
+        speed = serializedObject.FindProperty("speed");
+        offset = serializedObject.FindProperty("offset");
+        minRotation = serializedObject.FindProperty("minRotation");
+        maxRotation = serializedObject.FindProperty("maxRotation");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        /*
+        serializedObject.Update();
+
+        EditorGUILayout.LabelField("Camera movement speed:");
+        EditorGUILayout.PropertyField(speed);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Offset to screenborder for movement:");
+        EditorGUILayout.PropertyField(offset);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Minimum and maximum rotation of the camera:");
+        EditorGUILayout.PropertyField(minRotation);
+        EditorGUILayout.PropertyField(maxRotation);
+
+        EditorGUILayout.Space();
+
+        serializedObject.ApplyModifiedProperties();
+        */
+
+        DrawDefaultInspector();
+    }
+}
