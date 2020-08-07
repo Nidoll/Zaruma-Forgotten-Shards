c8a84e5e15a5af61fc31e1398e7b02bb6163ef64
Tristan Mark Nuck <nucktris@b-tu.de>
2020-08-07 21:04:17 +0200

    Units

---
+++ Zaruma-Forgotten-Shards-UnityProject/Assets/Units/EnemyUnit/EnemyUnit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnit : Unit
+{
+    public GameObject patrolCube1;
+    public GameObject patrolCube2;
+
+    public bool point1;
+    public bool stop;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        manager.AddEnemyUnit(this);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stop = agent.isStopped;
+
+        if(agent.isStopped){
+            if(point1){
+                agent.SetDestination(patrolCube1.transform.position);
+            }else{
+                agent.SetDestination(patrolCube2.transform.position);
+            }
+        }
+    }
+}
---
+++ Zaruma-Forgotten-Shards-UnityProject/Assets/Units/PlayerUnit/PlayerUnit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnit : Unit
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        manager.AddPlayerUnit(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
