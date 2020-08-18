1bf623857ef2b7b12123f3fb77c38cc51dc50f81
Tristan Mark Nuck <nucktris@b-tu.de>
2020-08-18 15:43:52 +0200

    Terrain Tools

--- Zaruma-Forgotten-Shards-UnityProject/Assets/Units/Player/Worker/PlayerWorker.cs
+++ Zaruma-Forgotten-Shards-UnityProject/Assets/Units/Player/Worker/PlayerWorker.cs
@@ -7,12 +7,18 @@ public class PlayerWorker : PlayerMeleeUnit
     private bool mining;
     private bool holdCrystal;
     private GameObject miningTarget;
+    public Mesh crystalMesh;
+    public Material crystalMat;
+    private MeshRenderer meshRend;
+    private MeshFilter meshFilter;
 
     // Start is called before the first frame update
     new void Start()
     {
         base.Start();
         StopCoroutine(CheckForEnemys());
+        meshRend = transform.Find("HoldingObject").gameObject.GetComponent<MeshRenderer>();
+        meshFilter = transform.Find("HoldingObject").gameObject.GetComponent<MeshFilter>();
     }
 
     // Update is called once per frame
@@ -26,5 +32,30 @@ public class PlayerWorker : PlayerMeleeUnit
         Debug.Log("Start Mining");
         ResetUnit();
         agent.SetDestination(target.transform.position);
+    }
+
+    public void HoldCrystal()
+    {
+        meshRend.material = crystalMat;
+        meshFilter.mesh = crystalMesh;
+    }
+
+    public void HoldNothing()
+    {
+        meshRend.material = null;
+        meshFilter.mesh = null;
+    }
+
+    IEnumerator CalcMining(GameObject target)
+    {
+        bool needOre = holdCrystal;
+
+        while(mining){
+            
+
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
     }
 }
